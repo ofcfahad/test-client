2bd5cb034266ccc9cba6d8a367adbfe5d3aeb45d
Fahad <fahadbilalbhatti16@gmail.com>
2023-08-11 12:46:50 +0500

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,16 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-const { REACT_APP_SERVER_URL } = process.env
+//const { REACT_APP_SERVER_URL } = process.env
+
+const server = 'https://project-management-server-rose.vercel.app'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/server': {
-        target: REACT_APP_SERVER_URL,
+        target: server,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/server/, ''),
       },
